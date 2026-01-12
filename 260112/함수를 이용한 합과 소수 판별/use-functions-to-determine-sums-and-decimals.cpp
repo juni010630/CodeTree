f037e1c47a257b6079f761340f9e83cf93ff499e
PPak JJun <juni010630@naver.com>
2026-01-12 19:02:38 +0900

    Title: 함수를 이용한 합과 소수 판별, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260112/함수를 이용한 합과 소수 판별/use-functions-to-determine-sums-and-decimals.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+int a, b;
+
+bool IsPrime(int n) {
+    for (int i=2; i<n; i++) {
+        if (!(n%i)) return false;
+    }
+    return true;
+}
+
+bool IsSumEven(int n) {
+    int sum{};
+    while(n) {
+        sum += n % 10;
+        n /= 10;
+    }
+    return !(sum%2);
+}
+
+int main() {
+    cin >> a >> b;
+
+    int cnt{};
+    while(a++ != b) {
+        if (IsPrime(a) && IsSumEven(a)) cnt++;
+    }
+
+    cout << cnt << endl;
+
+    return 0;
+}
