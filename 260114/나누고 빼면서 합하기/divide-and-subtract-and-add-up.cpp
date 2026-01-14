a613b9d7a6093dddd97d84a4eeabf870fc0659e0
PPak JJun <juni010630@naver.com>
2026-01-14 14:19:57 +0900

    Title: 나누고 빼면서 합하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 260114/나누고 빼면서 합하기/divide-and-subtract-and-add-up.cpp
+++ 260114/나누고 빼면서 합하기/divide-and-subtract-and-add-up.cpp
@@ -3,29 +3,26 @@
 using namespace std;
 
 int n, m;
-int A[100];
+int A[101];
 
 int func() {
-    int sum{};
-    while (m != 1) {
+    int sum = 0;
+    while (true) {
+        sum += A[m];
+        if (m == 1) break;
+
         if (m % 2 == 0) m /= 2;
         else m -= 1;
-        
-        sum += A[m];
     }
-    
-    sum += A[1];
     return sum;
 }
 
 int main() {
     cin >> n >> m;
 
-    for (int i = 0; i < n; i++) {
+    for (int i = 1; i <= n; i++) {
         cin >> A[i];
     }
-
-    
     
     cout << func() << endl;
     return 0;
