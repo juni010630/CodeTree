25557d92cf566675a6f779e56bb9d078dd0487a9
PPak JJun <juni010630@naver.com>
2026-01-21 17:16:22 +0900

    Title: 재귀함수를 이용한 최댓값, Time: 1087ms, Memory: 0MB, Status: Time Limit Exceed - Codetree

---
+++ 260121/재귀함수를 이용한 최댓값/maximum-value-with-recursive-function.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+int arr[100];
+
+int func(int N) {
+    if (N == 0) return arr[N];
+    
+    if (arr[N] > func(N-1)) return arr[N];
+    return func(N-1);
+}
+
+int main() {
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    cout << func(n) << endl;
+
+    return 0;
+}
