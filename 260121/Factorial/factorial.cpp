c7adeb3163e07229a98a6c1caecccb0c85bbe11b
PPak JJun <juni010630@naver.com>
2026-01-21 17:03:49 +0900

    Title: Factorial, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260121/Factorial/factorial.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int N;
+
+int fact(int n) {
+    if (n == 1) return 1;
+    return fact(n-1)*n;
+}
+
+int main() {
+    cin >> N;
+
+    cout << fact(N) << endl;
+
+    return 0;
+}
